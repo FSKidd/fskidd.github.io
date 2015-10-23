42f67038ed9ec44a4a718040fc9e6aee6196c6e6
Skyler Kidd <f.s.kidd@gmail.com>
2015-10-22 19:20:38 -0700

    Keyboard input works

--- controller/controllers.js
+++ controller/controllers.js
@@ -7,6 +7,12 @@
 
   App.controller("HomeCtrl", ["$scope", function ($scope){
     $scope.titleread = "skyler kidd";
+
+    $scope.insertchar = function(c) {
+      if ($scope.titleread.length < 16) {
+        $scope.titleread = $scope.titleread + c;
+      }
+    }
   }]);
 
   App.controller("AboutCtrl", ["$scope", function ($scope){
