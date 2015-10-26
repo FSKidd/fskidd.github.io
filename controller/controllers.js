2b7210e2f1b3c26d84b3ce96a93575e8c1963491
Skyler Kidd <f.s.kidd@gmail.com>
2015-10-25 21:55:21 -0700

    Console shifting

--- controller/controllers.js
+++ controller/controllers.js
@@ -10,14 +10,25 @@
     $scope.insertchar = function(c) {
       if ($scope.console_read[0].length < 16) {
         $scope.console_read[0] = $scope.console_read[0] + c;
-      }
-    }
-
+      };
+    };
     $scope.deletechar = function() {
       if ($scope.console_read[0].length > 0) {
         $scope.console_read[0] = $scope.console_read[0].substring(0, $scope.console_read[0].length - 1);
-      }
-    }
+      };
+    };
+    $scope.shift_console = function() {
+      for (var i = $scope.console_read.length - 1; i > 0; i--) {
+        $scope.console_read[i] = $scope.console_read[i-1];
+      };
+      $scope.console_read[0] = "";
+    };
+    $scope.enter = function() {
+      if ($scope.console_read[0].length > 0) {
+        // TODO
+        $scope.shift_console();
+      };
+    };
   }]);
 
   App.controller("AboutCtrl", ["$scope", function ($scope){
