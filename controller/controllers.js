83a7f5953c30fb358935defdb9d75bb23c783de1
Skyler Kidd <f.s.kidd@gmail.com>
2015-10-25 21:43:01 -0700

    Console window v1

--- controller/controllers.js
+++ controller/controllers.js
@@ -6,17 +6,16 @@
 
 
   App.controller("HomeCtrl", ["$scope", function ($scope){
-    $scope.titleread = "skyler kidd";
-
+    $scope.console_read = ["","Hello! Welcome to my site.","","",""];
     $scope.insertchar = function(c) {
-      if ($scope.titleread.length < 16) {
-        $scope.titleread = $scope.titleread + c;
+      if ($scope.console_read[0].length < 16) {
+        $scope.console_read[0] = $scope.console_read[0] + c;
       }
     }
 
     $scope.deletechar = function() {
-      if ($scope.titleread.length > 0) {
-        $scope.titleread = $scope.titleread.substring(0, $scope.titleread.length - 1);
+      if ($scope.console_read[0].length > 0) {
+        $scope.console_read[0] = $scope.console_read[0].substring(0, $scope.console_read[0].length - 1);
       }
     }
   }]);
